af2a16cde0648a8f6d93a563739eedfaaa7c50f9
debovema <mathieu.debove@fastconnect.fr>
2015-10-23 16:59:43 +0200

    Extract & deploy batch commands

---
+++ bw-maven-plugin/src/main/java/fr/fastconnect/factory/tibco/bw/maven/deployment/batch/AbstractBWBatchDeployMojo.java
@@ -0,0 +1,54 @@
+/**
+ * (C) Copyright 2011-2015 FastConnect SAS
+ * (http://www.fastconnect.fr/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.fastconnect.factory.tibco.bw.maven.deployment.batch;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.apache.maven.plugins.annotations.Parameter;
+
+import fr.fastconnect.factory.tibco.bw.maven.deployment.AbstractBWDeployMojo;
+
+public abstract class AbstractBWBatchDeployMojo extends AbstractBWDeployMojo {
+
+	protected final static String deployBatchDir = "deploy.batch.dir";
+	protected final static String deployBatchNoEAR = "deploy.batch.noear";
+	protected final static String deployBatchTemplate = "deploy.batch.template";
+	protected final static String deployBatchMin = "deploy.batch.min";
+	protected final static String deployBatchMax = "deploy.batch.max";
+	protected final static String deployBatchExportDeployed = "deploy.batch.exportDeployed";
+	protected final static String deployBatchNoStart = "deploy.batch.nostart";
+	protected final static String deployBatchNoStop = "deploy.batch.nostop";
+	protected final static String deployBatchForce = "deploy.batch.force";
+
+	/**
+	 * <p>
+	 * Directory to use for batch commands (<i>-dir</i> switch from AppManage).
+	 * </p>
+	 */
+	@Parameter ( property = deployBatchDir, required = true )
+	protected File dir;
+
+	@Override
+	public ArrayList<String> commonArguments() {
+		ArrayList<String> commonArguments = super.commonArguments();
+		commonArguments.add("-dir");
+		commonArguments.add(dir.getAbsolutePath());
+
+		return commonArguments;
+	}
+}
